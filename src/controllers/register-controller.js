0741115891bdc423e4e6cfa8c29c35f10c525c61
yurifardel <yrnascimento@outlook.com>
2021-08-04 19:00:39 -0300

    configuring the controller of register

--- src/controllers/register-controller.js
+++ src/controllers/register-controller.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const User = require("../../db/models/User");
 
 const route = express.Router();
 
 route.post("/register", async (req, res) => {
-  const { name, email, phone, cpf } = req.body;
+  try {
+    const { email } = req.body;
 
-  res.json({
-    name,
-    email,
-    phone,
-    cpf,
-  });
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "user already exists" });
+    }
+
+    const user = User.create(req.body);
+
+    return res.json({ user });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = route;
