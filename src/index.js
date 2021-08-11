31774aa9e59a2ff4f2e1252431ec25e1b0990e8a
yurifardel <yrnascimento@outlook.com>
2021-08-10 23:15:53 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const registerMongo = require("./controllers/register-controller");
+const authenticating = require("./controllers/authenticating");
 
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/auth", registerMongo);
+app.use("/auth", authenticating);
 
 app.listen(8080, () => {
   console.log("express listen in port 8080");
