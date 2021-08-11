551f7fbca635176b2fc932f2162a3690d712c22e
yurifardel <yrnascimento@outlook.com>
2021-08-10 23:12:34 -0300

    modifed

---
+++ db/models/utils/user.js
@@ -0,0 +1,49 @@
+const mongoose = require("../mongoose");
+const bcryptjs = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcryptjs.hash(this.password, 10);
+  this.password = hash;
+  this.confirmPassword = hash;
+
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
