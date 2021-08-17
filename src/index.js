57dbac1862bf88a651218e07b4fb07f3ef2c4006
yurifardel <yrnascimento@outlook.com>
2021-08-17 18:51:04 -0300

    up index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const authenticating = require("./controllers/authenticating");
+const addressController = require("./controllers/auth/address");
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/auth", authenticating);
+app.use("/auth", addressController);
 
 app.listen(8080, () => {
   console.log("express listen in port 8080");
