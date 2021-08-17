aaa3e5fee78a91945e4fe39771da9fa8ffc91d44
yurifardel <yrnascimento@outlook.com>
2021-08-17 18:28:49 -0300

    auth jwt

---
+++ src/middleware/authJwt.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const { getTokenFromHeaders, verifyJwt } = require("../utils/helpers/jwt/jwt");
+
+module.exports = (req, res, next) => {
+  const { url: path } = req;
+
+  const excludedPath = ["/register", "/login"];
+  const isExcludedpath = !!excludedPath.find((p) => p.startsWith(path));
+
+  if (isExcludedpath) return next();
+
+  const token = getTokenFromHeaders(req.headers);
+
+  if (!token) {
+    return res.status(401).send({ error: "no token provided" });
+  }
+
+  const parts = token.split(" ");
+
+  if (parts.length !== 2) {
+    return res.status(401).send({ error: "token error" });
+  }
+
+  const [bearer, tkn] = parts;
+
+  if (!/^Bearer$/i.test(bearer)) {
+    return res.status(401).send({ error: "token malformatted" });
+  }
+
+  try {
+    const decoded = verifyJwt(tkn);
+
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      return res.status(401).send({ error: "expired token" });
+    }
+  }
+};
