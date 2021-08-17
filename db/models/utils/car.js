41757f8face3b5b6d0afdafea070ef3f5970bfac
yurifardel <yrnascimento@outlook.com>
2021-08-17 18:27:44 -0300

    model

---
+++ db/models/utils/car.js
@@ -0,0 +1,29 @@
+const mongoose = require("../../mongoose");
+
+const CarSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  token: {
+    type: String,
+    select: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Car = mongoose.model("Car", CarSchema);
+
+module.exports = Car;
