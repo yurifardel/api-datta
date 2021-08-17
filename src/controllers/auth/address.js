1fdf8b22d358d3b8dc967d2493bd0241b88f55d7
yurifardel <yrnascimento@outlook.com>
2021-08-17 18:50:36 -0300

    created address register controller

--- src/controllers/auth/address.js
+++ src/controllers/auth/address.js
@@ -1,1 +1,16 @@
-const express = require("express");
+const { Router } = require("express");
+const ModelAddress = require("../../../db/models/utils/address");
+
+const route = Router();
+
+route.post("/register-address", async (req, res) => {
+  try {
+    const address = await ModelAddress.create(req.body);
+
+    return res.json({ address });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = route;
